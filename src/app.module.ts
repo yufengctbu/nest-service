d75ad6bed4851124a259a16d70e4188b4a83b3c6
even <yufengctbu@gmail.com>
2023-08-22 01:15:30 +0800

    feat(config): 添加项目公共的interface 以及常量

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { appConfig } from '@app/config';
+import { appConfig } from '@app/configs';
 
 @Module({
     imports: [
---
+++ src/constants/error-code.constant.ts
@@ -0,0 +1,7 @@
+enum COMMON {
+    PARAM_ERROR = 1, //参数错误
+}
+
+export const ERROR_CODE = {
+    COMMON,
+};

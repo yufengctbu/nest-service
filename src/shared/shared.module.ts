818bc388d1c2a3367ba17891c7c0eaa00fe5bbcd
even <yufengctbu@gmail.com>
2023-09-26 13:39:38 +0800

    feat(nodemailer):

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { LogModule } from './log/log.module';
 import { MysqlModule } from './mysql/mysql.module';
 import { RedisModule } from './redis/redis.module';
+import { EmailerModule } from './emailer/emailer.module';
 
 @Module({
-    imports: [LogModule, MysqlModule, RedisModule],
+    imports: [LogModule, MysqlModule, RedisModule, EmailerModule],
 })
 export class SharedModule {}
